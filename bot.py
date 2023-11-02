d8c2461d303c73cd9fc21200bc8ef7de71acac4c
ivalukyan <ivalukyanov@gmail.com>
2023-11-02 23:41:17 +0300

    new commands for bot

--- bot.py
+++ bot.py
@@ -4,3 +4,31 @@ from const import TOKEN
 
 # Token
 token = TOKEN
+
+# Initialized bot
+rcbo = telebot.TeleBot(TOKEN)
+
+# Comands (CRUD)
+@rcbo.message_handler(commands=['start'])
+def create_rcbo(message):
+    """
+    Function for create dialog
+    """
+    pass
+
+@rcbo.message_handler(commands=['update'])
+def update_rcbo(message):
+    """
+    Function for update dialog
+    """
+    pass
+
+@rcbo.message_handler(commands=['delete'])
+def delete_rcbo(message):
+    """
+    Function for delete dialog
+    """
+
+
+# Start bot
+rcbo.polling()
