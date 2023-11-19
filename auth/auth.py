12a720e8253d379bc8f0e0ee535a2066f7a22df9
ivalukyan <ivalukyanov@gmail.com>
2023-11-19 22:00:18 +0300

    updated auth

--- auth/auth.py
+++ auth/auth.py
@@ -1,15 +1,14 @@
 from const import ADM1, ADM2, ADM3, ADM4
-from bot import rcbo
-def auth(message):
+
+
+def auth_message(message):
     """
     Function for authentication
     """
 
     # Users who have admin status
     admin = [ADM1, ADM2, ADM3, ADM4]
-
-    MsgId  = rcbo.get_chat_member(message.from_user.id)
-    if MsgId in admin:
+    msgId = message.chat.id
+    if f'{msgId}' in admin:
         return True
-    else:
-        return False
+    return False
