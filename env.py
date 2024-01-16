00d69398fc30bea993e5b8b3e5c2e2177817e54e
ivalukyan <ivalukyanov@gmail.com>
2024-01-16 13:00:03 +0300

    fix

--- env.py
+++ env.py
@@ -11,8 +11,8 @@ ADM3 = os.getenv('ADM3')
 ADM4 = os.getenv('ADM4')
 TO_CHAT_ID = os.getenv('TO_CHAT_ID')
 
-NOTION_TOKEN = os.getenv('NOTION_TOKEN')
-NOTION_DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
+NOTION_TOKEN = os.getenv('NOTION_TOKEN_MY')
+NOTION_DATABASE_ID = os.getenv('NOTION_DATABASE_ID_MY')
 NOTION_URL = os.getenv('NOTION_URL')
 
 PATH = os.getenv('PATH')
